c233cdb2d22c51445038fa7ba99b96d6829738f0
Jinwoo <zvzv9808@gmail.com>
2024-01-03 23:34:43 +0900

    Initial Commit

---
+++ src/main/java/bros/flatcode/FlatcodeApplication.java
@@ -0,0 +1,13 @@
+package bros.flatcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bros/flatcode/FlatcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package bros.flatcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
