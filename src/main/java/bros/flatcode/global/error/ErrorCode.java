8cc1db72123e816d5a1118f9cb3a93dec1f2b213
Jinwoo <zvzv9808@gmail.com>
2024-01-07 11:41:30 +0900

    feat: ErrorCode 정의

---
+++ src/main/java/bros/flatcode/global/error/ErrorCode.java
@@ -0,0 +1,22 @@
+package bros.flatcode.global.error;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+
+    // 회원
+    INVALID_MEMBER_TYPE(HttpStatus.BAD_REQUEST, "M-001", "잘못된 회원 타입입니다. (memberType : KAKAO)"),
+    ALREADY_REGISTERED_MEMBER(HttpStatus.BAD_REQUEST, "M-002", "이미 가입된 회원입니다."),
+    USER_NOT_EXISTS(HttpStatus.BAD_REQUEST, "M-003", "해당 회원은 존재하지 않습니다.")
+    ;
+
+    ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    private HttpStatus httpStatus;
+    private String errorCode;
+    private String message;
+}
